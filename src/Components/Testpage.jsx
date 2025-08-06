db54447e692522ecb16e753d63858aa554a99bfd
Richard <109635077+RichyKim12@users.noreply.github.com>
2025-08-05 21:01:52 -0400

    Initialized React router

---
+++ src/Components/Testpage.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+function Testpage(){
+
+    return (
+        <>
+            <h1>Hello world</h1>
+        </>
+    );
+}
+
+export default Testpage
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Testpage from './Components/Testpage.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path:"/test",
+    element: <Testpage/>
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={route}/>
   </StrictMode>,
 )
