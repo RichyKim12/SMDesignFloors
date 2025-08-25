f10c0ea2550ce4400b69bc30576b2b80770c8ab6
Richard <109635077+RichyKim12@users.noreply.github.com>
2025-08-24 22:20:15 -0400

    start of the website

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./Navbar";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      
+      <div className="home-container">
+        <Navbar />
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the content of your homepage.</p>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import {AppBar, ToolBar} from "@mui/material";
+import React from "react";
+import { AppBar, Toolbar, Button, Typography, Box } from "@mui/material";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <>
-      <AppBar position='static'>
-        <ToolBar></ToolBar>
-      </AppBar>
-    </>
-  )
-}
+    <AppBar position="static" className="navbar">
+      <Toolbar>
+        <Typography variant="h6" className="navbar-title">
+          My Website
+        </Typography>
 
-export default Navbar
+        <Box>
+          <Button component={Link} to="/" className="navbar-link">
+            Home
+          </Button>
+          <Button component={Link} to="/contact-us" className="navbar-link">
+            Contact Us
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
