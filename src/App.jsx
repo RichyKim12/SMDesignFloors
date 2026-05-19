adcda34f241615cb95784515098e6b3dca679d39
Richard Kim <109635077+RichyKim12@users.noreply.github.com>
2026-05-19 17:43:35 -0400

    contractor dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './styles/global.css';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-
 // Existing components
 import Services from './Components/Services';
 import Professionals from './Components/Professionals';
@@ -11,12 +10,10 @@ import Contact from './Components/Contact';
 import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
 import FlooringVisualizer from './Components/FlooringVisualizer';
-
 // Auth
 import AuthModal from './Components/AuthModal';
 import ProtectedRoute from './Components/ProtectedRoute';
 import RoleRoute from './Components/RoleRoute';
-
 // Dashboards
 import ContractorDashboard from './pages/ContractorDashboard';
 import AdminDashboard from './pages/AdminDashboard';
@@ -40,8 +37,12 @@ function HomePage() {
   );
 }
 
+const DASHBOARD_ROUTES = ['/contractor', '/admin'];
+
 function Layout() {
   const [modalOpen, setModalOpen] = useState(false);
+  const location                  = useLocation();
+  const isDashboard               = DASHBOARD_ROUTES.includes(location.pathname);
 
   return (
     <>
@@ -50,9 +51,9 @@ function Layout() {
       <AuthModal isOpen={modalOpen} onClose={() => setModalOpen(false)} />
       <Routes>
         {/* Public routes */}
-        <Route path="/" element={<HomePage />} />
+        <Route path="/"            element={<HomePage />} />
         <Route path="/proservices" element={<Professionals />} />
-        <Route path="/visualizer" element={<FlooringVisualizer />} />
+        <Route path="/visualizer"  element={<FlooringVisualizer />} />
 
         {/* Protected routes */}
         <Route element={<ProtectedRoute />}>
@@ -64,7 +65,9 @@ function Layout() {
           </Route>
         </Route>
       </Routes>
-      <Footer />
+
+      {/* Hide footer on dashboard pages */}
+      {!isDashboard && <Footer />}
     </>
   );
 }
